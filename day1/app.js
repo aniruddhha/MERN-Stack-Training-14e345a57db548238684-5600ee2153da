fc4299ac7868df8c7285179b9e3758f3a4e12ede
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-04 11:52:17 +0530

    day1 - basics of js

--- day1/app.js
+++ day1/app.js
@@ -1,1 +1,38 @@
+/**
+ * This file explains basics of JS like 
+ *  - how to declare variables ?
+ *  - how to write functions ?
+ *  - how to call functions ?
+ *  - how to accumilate results
+ *  - evaluation .
+ */
+
+'use strict'
 console.log('Lets check')
+
+//num = 10
+
+var jk = 10
+console.log(jk)
+jk = 'data'
+console.log(jk)
+jk = true
+console.log(jk)
+
+function sayHi() {
+    console.log('Hi')
+}
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+function calc(str) {
+    return eval(str)
+}
+
+sayHi()
+var res = addition(12, 45)
+console.log(res)
+
+console.log(calc('103+2'))
