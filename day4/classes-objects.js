e6989c38484806936dcad0b29ce881aec094e5b5
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-18 11:26:09 +0530

    classes and objects intro

---
+++ day4/classes-objects.js
@@ -0,0 +1,12 @@
+class Car {
+    // properties
+    // behaviour --> act on propertes
+
+    speed = 10; // property
+
+    speedUp() { // behaviour
+        this.speed = this.speed + 10
+    }
+}
+
+const cr = new Car() // real object
