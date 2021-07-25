4dd6afaa2782df1a65ed7a9a1d77745f8dc3265d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-25 11:53:12 +0530

    nodejs : events once, promise, async, await

--- day6/main.js
+++ day6/main.js
@@ -2,12 +2,24 @@ const EventEmitter = require('events');
 
 const em = new EventEmitter()
 
-em.on(`invoices`, obj => {
+em.once(`invoices`, /*async*/ obj => { // listener function
+    console.log(`3`)
     console.log(`Just Got An Update`)
     console.log(this)
     console.log(obj)
+    obj.then(dt => console.log(dt))
+        .catch(err => console.log(err)) // recommended way
+
+    // const result = await obj
+    // console.log(result)
+})
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ inbNum: '1112-02-2021', invAmt: 1000 })
+    }, 5000)
 })
 
 setTimeout(() => { // used just for showing demo, it is not required actually
-    em.emit(`invoices`, { inbNum: '1112-02-2021', invAmt: 1000 })
+    em.emit(`invoices`, promise)
 }, 3000)
