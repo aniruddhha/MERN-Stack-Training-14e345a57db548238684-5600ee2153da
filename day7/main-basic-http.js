bc5630cb455f0ce01d22594492e90c6b40e71bac
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-08-01 12:30:02 +0530

    http : post method

--- day7/main-basic-http.js
+++ day7/main-basic-http.js
@@ -1,14 +1,37 @@
-const https = require('https') // t
+const https = require('https') // confirm the module
 
-const options = {
-    host: 'www.random.org',
-    path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new',
-    method: 'GET'
+// const options = {
+//     host: 'reqres.in',
+//     path: '/api/users?page=2'
+// }
+
+// https.request(options, res => {
+//     // console.log(res)
+//     let dt = ''
+//     res.on('data', chunk => dt += chunk)
+//     res.on('end', () => console.log(dt))
+// }).end()
+
+const data = new TextEncoder().encode(
+    JSON.stringify({ hi: 'hello' })
+)
+
+const opt = {
+    host: 'reqres.in',
+    path: '/api/users',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
 }
 
-https.request(options, res => {
-    // console.log(res)
+const req = https.request(opt, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
     let dt = ''
     res.on('data', chunk => dt += chunk)
     res.on('end', () => console.log(dt))
-}).end()
+})
+req.write(data)
+req.end()
