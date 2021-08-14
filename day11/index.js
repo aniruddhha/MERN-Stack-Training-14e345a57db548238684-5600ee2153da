191a7de88630952f704bc863058ec274eed84d7b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-08-14 12:23:32 +0530

    handlebars, layout and custom helpers

---
+++ day11/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+
+var app = express();
+
+hbs.registerHelper('foo', function (v1, v2, options) {
+    console.log(v1, v2)
+    console.log(options)
+    if (v1 > v2) return options.fn(this)
+    return options.inverse(this)
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+
+app.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' })
+})
+
+app.get('/about', (req, res, next) => {
+    res.render('about', { title: 'Express' })
+})
+
+app.get('/list', (req, res, next) => {
+
+    const mobiles = [
+        { os: 'android', price: 10 },
+        { os: 'iOS', price: 60 },
+        { os: 'windows', price: 20 },
+        { os: 'symbian', price: 15 },
+        { os: 'linux', price: 8 },
+    ]
+    res.render('list', { mobiles })
+})
+
+app.listen(3000, () => {
+    console.log(`Server Started on 3000`)
+})
