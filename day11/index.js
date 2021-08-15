f2d80805670df6255d04770b5d7b72e8d8999f75
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-08-15 09:23:17 +0530

    express : form submission

--- day11/index.js
+++ day11/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+// const bodyParser = require('body-parser')
 
 var app = express();
 
@@ -15,9 +16,9 @@ hbs.registerHelper('foo', function (v1, v2, options) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use(express.urlencoded());
 
 app.get('/', (req, res, next) => {
     res.render('index', { title: 'Express' })
@@ -37,6 +38,17 @@ app.get('/list', (req, res, next) => {
         { os: 'linux', price: 8 },
     ]
     res.render('list', { mobiles })
+})
+
+app.get('/save-form', (req, res, next) => {
+    res.render('save')
+})
+
+app.post('/save', (req, res) => {
+
+    // perform form processing e.g. saving form data to database
+    // res.send({ sts: 'success', body: req.body })
+    res.render('save')
 })
 
 app.listen(3000, () => {
