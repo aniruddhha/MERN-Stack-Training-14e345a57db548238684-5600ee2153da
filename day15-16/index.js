45fabf4553a87767655b15b2660ecf8c1052c909
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-08-29 09:46:10 +0530

    safe update

--- day15-16/index.js
+++ day15-16/index.js
@@ -2,19 +2,13 @@ const express = require('express')
 const BillingRepository = require('./billing/repository/billing.repo')
 const mysqlConfig = require('./configuration/mysql.config')
 
-
 const billRepo = new BillingRepository(mysqlConfig.connection)
-
-// const mySqlConfig = require('./configuration/mysql.config.middleware')
-
 const billingRouter = require('./billing/controller/billing.controller')(billRepo)
 
 const app = express()
 app.use(express.json())
-// app.use(mySqlConfig)  -- invoked per request
 app.use('/api/billing', billingRouter)
 app.use((err, req, res, next) => {
-    // global error handler 
     console.log(err)
     res.status(500)
     res.json({ sts: 'fail', msg: 'internal server error', url: req.url, err: err.stack })
