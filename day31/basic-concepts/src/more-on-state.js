4846b6c218d9356704879cd880d2348b69679887
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-10-31 09:21:19 +0530

    setState using function

--- day31/basic-concepts/src/more-on-state.js
+++ day31/basic-concepts/src/more-on-state.js
@@ -2,9 +2,30 @@ import { useState } from 'react'
 
 const MoreOfState = () => {
 
+    const [dt, setDt] = useState(10)
+
+    const onClk = () => {
+
+        setTimeout(() => {
+            setDt(prevVal => {
+                console.log(`Previous ${prevVal}`)
+                return 200
+            })
+        }, 1500)
+
+        setDt(400)
+
+        setDt(pv => {
+            if (pv < 100) return 200
+            else 600
+        })
+    }
+
     return (
         <div>
-            <h1>More on States</h1>
+            <h1>More on States {dt}</h1>
+
+            <button onClick={onClk}>Okay</button>
         </div>
     )
 }
